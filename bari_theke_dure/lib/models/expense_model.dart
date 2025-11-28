8478a42290c7e7715d962eb00e01480809f2870d
Sayeda Khadija Rahman <sayedakhadijarahman@gmail.com>
2025-11-28 18:32:00 +0600

    New Updates

---
+++ bari_theke_dure/lib/models/expense_model.dart
@@ -0,0 +1,27 @@
+class Expense {
+  final String id;
+  final String category;
+  final double amount;
+  final DateTime date;
+
+  Expense({
+    required this.id,
+    required this.category,
+    required this.amount,
+    required this.date,
+  });
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'category': category,
+        'amount': amount,
+        'date': date.toIso8601String(),
+      };
+
+  factory Expense.fromJson(Map<String, dynamic> json) => Expense(
+        id: json['id'],
+        category: json['category'],
+        amount: json['amount'],
+        date: DateTime.parse(json['date']),
+      );
+}

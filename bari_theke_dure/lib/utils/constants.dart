ea33ead611b0373b84f9c88dfc96c239fbcc043a
Sayeda Khadija Rahman <sayedakhadijarahman@gmail.com>
2025-11-28 18:30:12 +0600

    New Updates

---
+++ bari_theke_dure/lib/utils/constants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const List<Map<String, dynamic>> categories = [
+  {"name": "Food", "icon": Icons.fastfood, "color": Colors.red},
+  {"name": "Transportation", "icon": Icons.directions_bus, "color": Colors.blue},
+  {"name": "Shopping", "icon": Icons.shopping_bag, "color": Colors.purple},
+  {"name": "Education", "icon": Icons.school, "color": Colors.teal},
+  {"name": "Health", "icon": Icons.local_hospital, "color": Colors.green},
+  {"name": "Donation", "icon": Icons.volunteer_activism, "color": Colors.orange},
+  {"name": "Entertainment", "icon": Icons.movie, "color": Colors.pink},
+  {"name": "Others", "icon": Icons.more_horiz, "color": Colors.grey},
+];
+
+const Color primaryColor = Color(0xFF2E3A59);
+const Color accentColor = Color(0xFF4CAF50);
